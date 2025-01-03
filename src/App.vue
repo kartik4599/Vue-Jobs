c3a5d90f2ca4e6be1e8d247996fb4e1584bbeeb1
kartik10-24 <kartik.mendu@mindinventory.com>
2025-01-03 15:20:41 +0530

    Added Job Listing Component.

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,11 @@
 import Navbar from "./components/Navbar.vue";
 import Hero from "./components/Hero.vue";
 import HomeCards from "./components/HomeCards.vue";
+import JobListing from "./components/JobListing.vue";
 </script>
 <template>
   <Navbar />
   <Hero />
   <HomeCards />
+  <JobListing :limit="3" :show-button="true" />
 </template>
